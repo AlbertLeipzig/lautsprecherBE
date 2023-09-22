2bdc55da3db7727801b3fda3ef5c934d23354849
AlbertLeipzig <albertclemvill@gmail.com>
2023-09-22 17:50:07 +0200

    re-prise

--- src/db-connect.js
+++ src/db-connect.js
@@ -1,21 +1,11 @@
-import "./config.js"
-import mongoose from "mongoose"
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const db = mongoose.connect("mongodb+srv://AlbertLoewe:MP7b8sUUbmprc409@cluster0.t5thd.mongodb.net/data?retryWrites=true&w=majority", {useNewUrlParser : true}) 
-
-// URINNEN
-// SENSITIVE DATA => never store stuff like this in code
-// connection strings, passwords, secrets
-
-// we need dotenv.config() to be executred BEFOE!
-
-const DB_URI = process.env.DB_URI;
-
-console.log({ DB_URI });
 mongoose
-  .connect(DB_URI, {
+  .connect(process.env.DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('DB Connection estabished!'))
-  .catch((err) => console.log('[ERROR] Connection failed', err));
+  .then(() => console.log('DB Connection established!'))
+  .catch((err) => console.error('[ERROR] Connection failed', err));
--- src/index.js
+++ src/index.js
@@ -1,60 +1,47 @@
-/* const express = require("express") */
-import "./config.js";
-import "./db-connect.js";
-import morgan from "morgan";
-import express from "express";
-import cors from "cors";
-import bodyParser from "body-parser"
+import './config.js';
+import './db-connect.js';
+import morgan from 'morgan';
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
-import actualArticleRoute from "./routes/actualArticleRoute.js";
-import historyArticleRoute from "./routes/historyArticleRoute.js";
-import bandRoute from "./routes/bandRoute.js";
-import musicianRoute from "./routes/musicianRoute.js";
-import concertRoute from "./routes/concertRoute.js";
-import musicbusinessRoute from "./routes/musicbusinessRoute.js";
-import teamRoute from "./routes/teamRoute.js";
-import veranstaltungsortRoute from "./routes/veranstaltungsortRoute.js";
-import subscriberRoute from "./routes/subscriberRoute.js";
-import messageRoute from "./routes/messageRoute.js";
+import actualArticleRoute from './routes/actualArticleRoute.js';
+import historyArticleRoute from './routes/historyArticleRoute.js';
+import bandRoute from './routes/bandRoute.js';
+import musicianRoute from './routes/musicianRoute.js';
+import concertRoute from './routes/concertRoute.js';
+import musicbusinessRoute from './routes/musicbusinessRoute.js';
+import teamRoute from './routes/teamRoute.js';
+import veranstaltungsortRoute from './routes/veranstaltungsortRoute.js';
+import subscriberRoute from './routes/subscriberRoute.js';
+import messageRoute from './routes/messageRoute.js';
 
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT || 4000
-const MENSAJE = process.env.MENSAJE
-const PWD = process.env.PASSWORD
+const PORT = process.env.PORT || 4000;
 
-app.use(cors({
-    origin: process.env.FRONTEND,
-    credentials: true
-}))
+app.use(
+  cors({
+    origin: process.env.FRONTEND_PORT,
+    credentials: true,
+  })
+);
 
-app.use(morgan("tiny"))
-/* body parser */
-app.use(express.json())
-app.use(bodyParser.json())
+app.use(morgan('tiny'));
+app.use(express.json());
+app.use(bodyParser.json());
 
-
-app.use("/api/actualarticle", actualArticleRoute)
-app.use("/api/historyarticle", historyArticleRoute)
-app.use("/api/subscribers", subscriberRoute)
-app.use("/api/musician", musicianRoute)
-app.use("/api/band", bandRoute)
-app.use("/api/concert", concertRoute)
-app.use("/api/veranstaltungsort", veranstaltungsortRoute)
-app.use("/api/musicbusiness", musicbusinessRoute)
-app.use("/api/team", teamRoute)
-app.use("/api/messages", messageRoute)
-
+app.use('/api/actualarticle', actualArticleRoute);
+app.use('/api/historyarticle', historyArticleRoute);
+app.use('/api/subscribers', subscriberRoute);
+app.use('/api/musician', musicianRoute);
+app.use('/api/band', bandRoute);
+app.use('/api/concert', concertRoute);
+app.use('/api/veranstaltungsort', veranstaltungsortRoute);
+app.use('/api/musicbusiness', musicbusinessRoute);
+app.use('/api/team', teamRoute);
+app.use('/api/messages', messageRoute);
 
 app.listen(PORT, () => {
-    console.log(`Server: http://localhost:${PORT}`)
-    console.log(`RECORDS: http://localhost:${PORT}/api/musician`)
-    console.log(`ACTUAL ARTICLE: http://localhost:${PORT}/api/actualarticle`)
-    console.log(`HISTORY ARTICLE: http://localhost:${PORT}/api/historyarticle`)
-    console.log(`BAND: http://localhost:${PORT}/api/band`)
-    console.log(`CONCERT: http://localhost:${PORT}/api/concert`)
-    console.log(`VERANSTALTUNGSORT:http://localhost:${PORT}/api/veranstaltungsort`)
-    console.log(`MUSICBUSINESS: http://localhost:${PORT}/api/musicbusiness`)
-    console.log(`TEAM: http://localhost:${PORT}/api/team`)
-    console.log(`TEAM: http://localhost:${PORT}/api/messages`)
-})
+  console.log(`server running on port ${PORT}`);
+});
